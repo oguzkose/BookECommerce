102575ba63163878c64a0fcc857abf2499ea9cd1
oguzkose <oguzkose2019@outlook.com>
2021-06-08 00:00:06 +0300

    Entity'ler ve Context eklendi. Migration yapıldı

---
+++ ECommerce.Data/Context/ECommerceDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Data.Context
+{
+    public class ECommerceDbContext:DbContext
+    {
+        public ECommerceDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ECommerce.Data/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Book { get; set; }
+    }
+}
