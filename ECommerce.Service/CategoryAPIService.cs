33ee3f67d3407e69c508ab4605c395ee4c19a745
oguzkose <oguzkose2019@outlook.com>
2021-06-08 14:36:46 +0300

    Service Katmanı eklendi, API için CategoryService oluşturuldu , GetAll metodu yazıldı.

---
+++ ECommerce.Service/CategoryAPIService.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Data.Context;
+using ECommerce.Service.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Service
+{
+    public class CategoryAPIService
+    {
+        private readonly ECommerceDbContext _context;
+        public CategoryAPIService(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Tüm kategorileri getiren metot
+        /// </summary>
+        /// <returns>List<CategoryResponse></returns>
+        public List<CategoryResponse> GetAllCategories()
+        {
+            var category = _context.Categories
+                .Select(x => new CategoryResponse
+            {
+                Id = x.Id,
+                Name = x.Name
+
+            }).ToList();
+
+            return category;
+        }
+    }
+}
